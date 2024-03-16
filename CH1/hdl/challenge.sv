f70ff575aacefcfc76eeb4a761c2583329c1842b
Pedro Rodríguez <prodriguezvieites@gmail.com>
2024-03-16 18:23:42 +0100

    update ch1

--- CH1/hdl/challenge.sv
+++ CH1/hdl/challenge.sv
@@ -1,13 +1,13 @@
 `timescale 1ns/10ps
 module challenge
-  (
-   input  wire  [2:0]    SW,
-   output logic [1:0]    LED
-   );
+(
+  input  wire  [2:0]    SW,
+  output logic [1:0]    LED
+  );
 
-  // SW[2] is carry in
-  // SW[1] is A
-  // SW[0] is B
-  assign LED[0]  = ; // Write the code for the Sum
-  assign LED[1]  = ; // Write the code for the Carry
+ // SW[2] is carry in
+ // SW[1] is A
+ // SW[0] is B
+ assign LED[0]  = SW[0] ^ SW[1] ^ SW[2]; // Write the code for the Sum
+ assign LED[1]  = (SW[0] && SW[1]) || (SW[0] && SW[2]) || (SW[1] && SW[2]); // Write the code for the Carry
 endmodule // challenge
